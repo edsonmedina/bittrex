14d30bebb299fe620bebb34351d2fc994d47f468
Edson Medina <emedina@datix.co.uk>
2017-06-26 01:00:59 +0100

    Removed unused import

--- test.php
+++ test.php
@@ -3,7 +3,6 @@
 require __DIR__.'/vendor/autoload.php';
 
 use edsonmedina\bittrex\Account;
-use edsonmedina\bittrex\valueobjects\Market;
 
 $apiKey = '9e84f91bdd9c4d1486a5393ada5af523';
 $secret = '39b5b5f00a5b47b5a62439bd7b0216b6';
